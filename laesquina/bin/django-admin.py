a5c065f47528ba82fe0294aa6045993346d5321e
JPGarrido <jpgarridoinformatica@gmail.com>
2024-04-05 21:42:54 -0300

    primer commit

---
+++ laesquina/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jpgarrido/proyectos_python/desafio_entorno_virtual/laesquina/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
